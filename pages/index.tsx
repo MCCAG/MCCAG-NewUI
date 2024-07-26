cb87b7a053f61ca5676381e133ccab285106d76e
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-07-26 20:33:12 +0800

    feat: add user projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,33 @@
-import {Box, Card, Center, Container, SimpleGrid, Space, Stack, Tabs, Text} from '@mantine/core';
-import React from 'react';
+import {
+    Box,
+    Card,
+    Center,
+    Container,
+    SimpleGrid,
+    Space,
+    Stack,
+    Tabs,
+    Text,
+    ScrollArea,
+    Group,
+    Button
+} from '@mantine/core';
+import React, {useState} from 'react';
 import { OfficialIdSkin } from '@/components/OfficialIdSkin/OfficialIdSkin';
 import tabsStyle from './tab.module.css';
-import { Footer } from '@/components/Footer/Footer';
 import SkinServerSkin from '@/components/SkinServerSkin/SkinServerSkin';
 import UploadedSkin from '@/components/UploadedSkin/UploadedSkin';
+import {ImageCard} from "@/components/ImageCard/ImageCard";
 
 export default function HomePage() {
-    // @ts-ignore
-    // @ts-ignore
+    let [showLeast, setShowLeast] = useState(true);
+
     return (
         <Container maw={850}>
             <Space h="10vh" />
             <Center maw={850}>
                 <SimpleGrid cols={{ base: 1, lg: 2 }} spacing={50}>
-                    <Card shadow="sm" padding="lg" radius="lg" withBorder>
+                    <Card shadow="sm" padding="lg" radius="lg" withBorder h={540}>
                         <Card.Section>
                             <Tabs defaultValue="genuine_id" classNames={tabsStyle} variant="unstyled">
                                 <Tabs.List style={{ height: 40 }} grow>
@@ -48,24 +61,44 @@ export default function HomePage() {
                         </Card.Section>
                     </Card>
 
-                    <Card shadow="sm" padding="lg" radius="lg" withBorder>
+                    <Card shadow="sm" padding="lg" radius="lg" withBorder h={540}>
                         <Card.Section
-                            shadow="sm"
-                                      padding="lg"
-                                      radius="lg"
-                                      style={{backgroundColor: "rgba(83, 146, 239, 0.3)", height: 40}}
-                                      withBorder>
+                          shadow="sm"
+                          padding="lg"
+                          radius="lg"
+                          style={{ backgroundColor: 'rgba(83, 146, 239, 0.3)', height: 40 }}
+                          withBorder>
                             <Center h={40}>
-                                <Text style={{color: "var(--mantine-color-blue-filled)", fontSize: 18, fontWeight: "bold"}}>
+                                <Text style={{ color: 'var(--mantine-color-blue-filled)', fontSize: 18, fontWeight: 'bold' }}>
                                     用户作品
                                 </Text>
                             </Center>
                         </Card.Section>
                         <Box>
                             <Space h={30} />
-                            <SimpleGrid>
-
-                            </SimpleGrid>
+                            <Center>
+                                <Stack>
+                                    <ScrollArea h={"auto"}>
+                                        <SimpleGrid cols={2}>
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                            <ImageCard url={"https://picsum.photos/130/130"} />
+                                        </SimpleGrid>
+                                    </ScrollArea>
+                                    <Group justify={"center"}>
+                                        <Button variant={showLeast ? "filled": "subtle"} onClick={() => {
+                                            setShowLeast(true);
+                                        }}>最新</Button>
+                                        <Button variant={showLeast ? "subtle": "filled"} onClick={() => {
+                                            setShowLeast(false);
+                                        }}>最热</Button>
+                                        <Button>换一批</Button>
+                                    </Group>
+                                </Stack>
+                            </Center>
                         </Box>
                     </Card>
                 </SimpleGrid>
