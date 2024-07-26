975da0781c42544f326af3e44784b81bc1b3cd14
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-07-27 00:47:44 +0800

    feat: load image

--- components/OfficialIdSkin/OfficialIdSkin.tsx
+++ components/OfficialIdSkin/OfficialIdSkin.tsx
@@ -1,12 +1,21 @@
-import { Button, Stack, TextInput, Group, Image } from '@mantine/core';
+'use client';
+
+import { Button, Stack, TextInput, Group, Image, LoadingOverlay } from '@mantine/core';
+import { useState } from 'react';
+import { SERVER_HOME } from '@/components/ServerProperties';
 
 export function OfficialIdSkin() {
+    const [input, setInput] = useState('');
+    const [imageLink, setImageLink] = useState('');
+    const [loading, setLoading] = useState(false);
+
     return (
         <Stack
           align="center"
           justify="center"
           gap="md"
         >
+            <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ blur: 2 }} />
             <TextInput
               size="md"
               placeholder="正版 ID"
@@ -16,9 +25,18 @@ export function OfficialIdSkin() {
                     borderRadius: '8px',
                 }}
               w={290}
+              value={input}
+              onChange={(e) => {
+                    setInput(e.currentTarget.value);
+                }}
             />
             <Group w={290} justify="space-between">
-                <Button>生成</Button>
+                <Button onClick={() => {
+                    setLoading(true);
+                    setImageLink(`${SERVER_HOME}/loader/minecraft.net/player/${input}`);
+                }}>
+                    生成
+                </Button>
                 <Button variant="subtle">换背景</Button>
                 <Button variant="subtle">下载</Button>
             </Group>
@@ -29,8 +47,14 @@ export function OfficialIdSkin() {
               style={{
                     boxShadow: '0 0 0 2px #5392EF',
                 }}
-              // src="https://picsum.photos/290/290"
+              src={imageLink}
               fallbackSrc="/square.jpg"
+              onLoad={() => {
+                    setLoading(false);
+                }}
+              onError={(e) => {
+                  console.log(e);
+              }}
             />
             <Group>
                 <Button variant="subtle">发布作品</Button>
---
+++ components/ServerProperties.ts
@@ -0,0 +1,1 @@
+export const SERVER_HOME = 'https://laptop.devtest.xiaojin233.cn';
